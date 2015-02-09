70fc7c259c31c0340ee4161c12fd1cab18d75b20
rjsteinert <rj@rjsteinert.com>
2015-02-09 02:36:00 -0500

    Add  and  commands

--- pipe.js
+++ pipe.js
@@ -2,14 +2,27 @@
 
 var program = require('commander');
 var fs = require('fs')
-var config = JSON.parse(fs.readFileSync('config.json', 'utf8'))
 var exec = require('child_process').exec;
 
 var hello = function () {
   console.log('Hello. I am thing.')
 }
 
+var watch = function () {
+  var config = JSON.parse(fs.readFileSync('config.json', 'utf8'))
+  var interval = parseInt(config.interval)
+  setInterval(function() {
+    exec('pipe send `pipe poll`', function(err, stdout, stderr) {
+    //exec('pipe poll', function(err, stdout, stderr) {
+      if (err) console.log(err)
+      if (stdout) console.log(stdout)
+      if (stderr) console.log(stderr)
+    })
+  }, config.interval)
+}
+
 var log = function () {
+  var config = JSON.parse(fs.readFileSync('config.json', 'utf8'))
   exec('./node_modules/' + config.thing + '/poll', function(err, stdout, stderr) {
     if (err) console.log(err)
     if (stdout) console.log(stdout)
@@ -18,6 +31,7 @@ var log = function () {
 }
 
 var send = function (message) {
+  var config = JSON.parse(fs.readFileSync('config.json', 'utf8'))
   exec('./node_modules/' + config.reservoir + '/send "' + message + '"', function(err, stdout, stderr) {
     if (err) console.log(err)
     if (stdout) console.log(stdout)
@@ -26,6 +40,7 @@ var send = function (message) {
 }
 
 var poll = function () {
+  var config = JSON.parse(fs.readFileSync('config.json', 'utf8'))
   exec('./node_modules/' + config.thing + '/poll', function(err, stdout, stderr) {
     if (err) console.log(err)
     if (stdout) console.log(stdout)
@@ -34,6 +49,7 @@ var poll = function () {
 }
 
 var setReservoir = function (reservoirName) {
+  var config = JSON.parse(fs.readFileSync('config.json', 'utf8'))
   console.log('setting default reservoir to %s', reservoirName);
   config.reservoir = reservoirName
   fs.writeFile('config.json', JSON.stringify(config), function(err) {
@@ -43,8 +59,19 @@ var setReservoir = function (reservoirName) {
 }
 
 var setThing = function (thingName) {
+  var config = JSON.parse(fs.readFileSync('config.json', 'utf8'))
   console.log('setting default thing to %s', thingName);
   config.thing = thingName
+  fs.writeFile('config.json', JSON.stringify(config), function(err) {
+    if (err) return console.log(err)
+    console.log('done')
+  })
+}
+
+var setWatchInterval = function (intervalLength) {
+  var config = JSON.parse(fs.readFileSync('config.json', 'utf8'))
+  console.log('setting interval to %s', intervalLength);
+  config.interval = intervalLength
   fs.writeFile('config.json', JSON.stringify(config), function(err) {
     if (err) return console.log(err)
     console.log('done')
@@ -72,8 +99,17 @@ program
   .action(setReservoir)
 
 program
+  .version('0.0.1')
+  .command('interval <intervalLength>')
+  .action(setWatchInterval)
+
+program
   .command('init')
   .action(init)
+
+program
+  .command('watch')
+  .action(watch)
 
 program
   .command('poll')
@@ -82,11 +118,6 @@ program
 program
   .version('0.0.1')
   .command('send <data>')
-  .action(send)
-
-program
-  .version('0.0.1')
-  .command('log')
   .action(send)
 
 program
