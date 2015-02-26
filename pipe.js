4c2bc3052456008c01bf8cb0d7bd26eabcc2f684
R.J. Steinert <rj@rjsteinert.com>
2015-02-26 16:50:34 -0500

    Remove code being deprecated and reorganize

--- pipe.js
+++ pipe.js
@@ -21,73 +21,6 @@ var bootstrap = function(callback) {
   })
 }
 
-var hello = function () {
-  console.log('Hello. I am thing.')
-}
-
-var watch = function () {
-  var interval = parseInt(config.interval)
-  setInterval(function() {
-    exec('pipe send `pipe poll`', function(err, stdout, stderr) {
-    //exec('pipe poll', function(err, stdout, stderr) {
-      if (err) console.log(err)
-      if (stdout) console.log(stdout)
-      if (stderr) console.log(stderr)
-    })
-  }, config.interval)
-}
-
-var log = function () {
-  exec('./node_modules/' + config.thing + '/poll', function(err, stdout, stderr) {
-    if (err) console.log(err)
-    if (stdout) console.log(stdout)
-    if (stderr) console.log(stderr)
-  })
-}
-
-var send = function (message) {
-  exec('./node_modules/' + config.reservoir + '/send "' + message + '"', function(err, stdout, stderr) {
-    if (err) console.log(err)
-    if (stdout) console.log(stdout)
-    if (stderr) console.log(stderr)
-  })
-}
-
-var poll = function () {
-  exec('./node_modules/' + config.thing + '/poll', function(err, stdout, stderr) {
-    if (err) console.log(err)
-    if (stdout) console.log(stdout)
-    if (stderr) console.log(stderr)
-  })
-}
-
-var setReservoir = function (reservoirName) {
-  console.log('setting default reservoir to %s', reservoirName);
-  config.reservoir = reservoirName
-  fs.writeFile('config.json', JSON.stringify(config), function(err) {
-    if (err) return console.log(err)
-    console.log('done')
-  })
-}
-
-var setThing = function (thingName) {
-  console.log('setting default thing to %s', thingName);
-  config.thing = thingName
-  fs.writeFile('config.json', JSON.stringify(config), function(err) {
-    if (err) return console.log(err)
-    console.log('done')
-  })
-}
-
-var setWatchInterval = function (intervalLength) {
-  console.log('setting interval to %s', intervalLength);
-  config.interval = intervalLength
-  fs.writeFile('config.json', JSON.stringify(config), function(err) {
-    if (err) return console.log(err)
-    console.log('done')
-  })
-}
-
 var init = function () {
   bootstrap(function() {
     console.log('initializing your pipe')
@@ -156,11 +89,14 @@ var test = function() {
 
 }
 
+var hello = function () {
+  console.log('Hello. I am thing.')
+}
 
 program
-  .command('test')
-  .action(test)
-  
+  .command('init')
+  .action(init)
+
 program
   .command('clone <repoLocation>')
   .action(clone)
@@ -170,36 +106,8 @@ program
   .action(driver)
 
 program
-  .version('0.0.1')
-  .command('thing <thingName>')
-  .action(setThing)
-
-program
-  .version('0.0.1')
-  .command('reservoir <reservoirName>')
-  .action(setReservoir)
-
-program
-  .version('0.0.1')
-  .command('interval <intervalLength>')
-  .action(setWatchInterval)
-
-program
-  .command('init')
-  .action(init)
-
-program
-  .command('watch')
-  .action(watch)
-
-program
-  .command('poll')
-  .action(poll)
-
-program
-  .version('0.0.1')
-  .command('send <data>')
-  .action(send)
+  .command('test')
+  .action(test)
 
 program
   .command('*')
